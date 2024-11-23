d172b7ec6d98af1c4ea8674f97169f52cb4e8e46
Juan Avendaño <javendano.n@gmail.com>
2024-11-23 15:51:41 -0300

    feat: implemented an adapter to send prompts to different clients and basic retry strategy

---
+++ clients/adapter.py
@@ -0,0 +1,29 @@
+
+from clients.anthropic import AnthropicClient
+import time
+
+
+class SendPromptToClientAdapter:
+    def __init__(self, clients):
+        self.clients = clients
+
+    def send_prompt(self, prompt):
+        for client in self.clients:
+            response = client.send_prompt(prompt)
+            if not response:
+                continue
+            return response
+
+
+def send_prompt_to_clients(prompt: str):
+    for attempt in range(3):
+        clients = [
+            AnthropicClient(),
+        ]
+        adapter = SendPromptToClientAdapter(
+            clients=clients
+        )
+        response = adapter.send_prompt(prompt=prompt)
+        if not response:
+            time.sleep((2 ** attempt))
+        return response
