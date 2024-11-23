c4142e18be7c32d40e2829f7d10b63e24abc0edc
Juan Avendaño <javendano.n@gmail.com>
2024-11-23 02:48:43 -0300

    feat: added functions to get works from open alex API, added inverted_idx_to_string function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
 from flask import Flask, request, jsonify
+from constants import OPEN_ALEX_API_URL
 import requests
 
 app = Flask(__name__)
@@ -10,10 +10,67 @@ def hello_world():
     return "hello platanus first commit!"
 
 
-@app.route('/query', methods=['POST'])
+def get_works(query: str):
+    response = requests.get(
+        f"{OPEN_ALEX_API_URL}/works?search={query}"
+    )
+    if response.status_code != 200:
+        return None
+
+    return response.json()
+
+
+def get_works_with_abstract(query: str):
+    response = requests.get(
+        f"{OPEN_ALEX_API_URL}/works?search={query}&filter=has_abstract:true"
+    )
+    if response.status_code != 200:
+        return None
+
+    return response.json()
+
+
+def inverted_index_to_text(inverted_idx: dict):
+    position_term_pairs = []
+    for term, positions in inverted_idx.items():
+        for position in positions:
+            position_term_pairs.append((position, term))
+
+    position_term_pairs.sort()
+    reconstructed_string = []
+
+    for _, term in position_term_pairs:
+        reconstructed_string.append(term)
+
+    return ' '.join(reconstructed_string)
+
+
+
+@app.route('/query', methods=['GET'])
 def query():
-    ...
+    query = request.args.get("query")
+    if not query:
+        return jsonify({"error": "query parameter is required"}), 400
+
+    works_with_abstract = get_works_with_abstract(
+        query=query
+    )
+    if not works_with_abstract or len(works_with_abstract["results"]) == 0:
+        return jsonify({"error": "failed to get works"}), 500
+
+    results = [
+        {
+            "doi": work["doi"],
+            "title": work["title"],
+            "abstract": inverted_index_to_text(
+                inverted_idx=work["abstract_inverted_index"]
+            )
+        }
+        for work in works_with_abstract["results"][0:15]
+    ]
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True)
