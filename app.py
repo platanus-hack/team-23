d1a35feaa6a0ad9d1c688ff1fd0b22f5cead5ec9
Juan Avendaño <javendano.n@gmail.com>
2024-11-23 17:21:47 -0300

    refactor: removed logic from other files in app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json
-from flask import Flask, request, jsonify
-from constants import OPEN_ALEX_API_URL, TAG_NAME, PER_PAGE_LIMIT
-from flask_cors import CORS
-from urllib.parse import urlencode
 import requests
+from flask import Flask, request, jsonify
+from constants import TAG_NAME, PER_PAGE_LIMIT
+from flask_cors import CORS
 from dotenv import load_dotenv
-import re
+from utils import extract_tag_content, get_first_n_words, inverted_index_to_text
 from clients.adapter import send_prompt_to_clients
+from open_alex_client import get_works_by_keywords
 
 
 load_dotenv()
@@ -16,62 +16,16 @@ app = Flask(__name__)
 cors = CORS(app)
 
 
-def get_works_by_keywords(keywords, per_page = 100):
-    query = ' OR '.join(keywords)
-    params = {
-        'search': query,
-        'per_page': per_page,
-        'filter': 'has_abstract:true',
-        'select': 'id,doi,title,authorships,abstract_inverted_index,publication_year',
-    }
-    encoded_query = urlencode(params)
-    url = f"{OPEN_ALEX_API_URL}/works?{encoded_query}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-
-    return response.json()
-
-
-def extract_tag_content(text, tag_name):
-    pattern = rf"<{tag_name}>(.*?)</{tag_name}>"
-    match = re.search(pattern, text, re.DOTALL)
-    if match:
-        return match.group(1).strip()
-    return None
-
-
 @app.route("/")
 def hello_world():
     return "hello platanus first commit!"
-
-
-def inverted_index_to_text(inverted_idx: dict):
-    position_term_pairs = []
-    for term, positions in inverted_idx.items():
-        for position in positions:
-            position_term_pairs.append((position, term))
-
-    position_term_pairs.sort()
-    reconstructed_string = []
-
-    for _, term in position_term_pairs:
-        reconstructed_string.append(term)
-
-    return ' '.join(reconstructed_string)
-
-
-def get_first_n_words(text: str, n: int = 300) -> str:
-    words = text.split()
-    first_200_words = words[:n]
-    return ' '.join(first_200_words)
 
 
 @app.route('/query', methods=['GET'])
 def query():
     works_partial = []
     question = request.args.get("question")
-    per_page = int(request.args.get("per_page", 100))
+    per_page = int(request.args.get("per_page", PER_PAGE_LIMIT))
     if per_page > PER_PAGE_LIMIT:
         per_page = PER_PAGE_LIMIT
 
@@ -87,8 +41,9 @@ def query():
     if not terms:
         return jsonify({"error": "failed to get terms"}), 500
 
-    works = get_works_by_keywords(keywords=terms["keywords"], per_page=per_page)
-
+    works = get_works_by_keywords(
+        keywords=terms["keywords"], per_page=per_page,
+    )
     works_partial = [
         {
             "doi": work["doi"],
