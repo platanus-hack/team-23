310248e41e75d7242645b3fd547255b5dcc137c7
Juan Avendaño <javendano.n@gmail.com>
2024-11-24 01:23:13 -0300

    chore: changed PER_PAGE_LIMIT from 50 to 30 in constants

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ OPEN_ALEX_API_URL = "https://api.openalex.org"
 OPEN_ALEX_DEFAULT_SELECT = "id,doi,title,authorships,abstract_inverted_index,publication_year,publication_date,cited_by_count"
 OPEN_ALEX_DEFAULT_FILTER = "has_abstract:true"
 TAG_NAME = "response"
-PER_PAGE_LIMIT = 50
+PER_PAGE_LIMIT = 30
 
 KEYWORDS_PROMPT_PATH = "./prompts/KEYWORDS_PROMPT.txt"
 SUMMARY_PROMPT_PATH = "./prompts/SUMMARY_PROMPT.txt"
