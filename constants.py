d76244c6fdf656cb05464696d9be26eaa99690b1
Juan Avendaño <javendano.n@gmail.com>
2024-11-23 21:16:58 -0300

    chore: added open alex constants

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 OPEN_ALEX_API_URL = "https://api.openalex.org"
+OPEN_ALEX_DEFAULT_SELECT = "id,doi,title,authorships,abstract_inverted_index,publication_year,publication_date,cited_by_count"
+OPEN_ALEX_DEFAULT_FILTER = "has_abstract:true"
 TAG_NAME = "response"
 PER_PAGE_LIMIT = 50
