8a7ab0c64150d260644c951949211f3aa992a90a
Juan Avendaño <javendano.n@gmail.com>
2024-11-24 05:54:17 -0300

    chore: added missing method in BaseClient

--- clients/base.py
+++ clients/base.py
@@ -1,10 +1,11 @@
-
-
 class BaseClient:
     CLIENT = None
     MODELS = []
     POWERFUL_MODELS = []
     LIGHT_MODELS = []
+
+    def get_models(self, use_powerful_model=False, use_light_model=False):
+        raise NotImplementedError
 
     def send_prompt(self, prompt, use_powerful_model=False, use_light_model=False):
         raise NotImplementedError
